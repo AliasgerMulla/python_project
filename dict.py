3136d5410ee4ae80dd943561239420a2f8c17e77
AliasgerMulla <143435669+AliasgerMulla@users.noreply.github.com>
2023-09-25 15:48:26 +0530

    Add files via upload
    Python Project main file

---
+++ dict.py
@@ -0,0 +1,19 @@
+Emp ={'1001':'Aliasger','1002':'Cleta','1003':'Eshan','1004':'Priya'}
+
+
+
+
+#Delete a dictionary
+Emp.pop()
+print(Emp)
+
+del Emp['1001']
+print(Emp)
+
+Emp.clear()
+print (Emp)
+
+
+
+
+
